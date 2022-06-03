96145e3c3ac3b743c25fae453d0c54abfeb63295
Diana Marcelin <marcelindiana@yahoo.com>
2022-06-03 18:24:25 -0400

    checkbox showing task that is complete--created edit button

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,85 @@
 <script>
 export default {
-  name: 'app',
+  name: "app",
   data() {
     return {
-      newTodo: '',
+      newTodo: "",
       todos: [
         {
           id: 1,
-          text: 'Learn Vue',
-          completed: false
+          text: "Buy Flowers",
+          completed: true,
         },
         {
           id: 2,
-          text: 'Complete Vue Project',
-          completed: false
+          text: "Complete Vue Project",
+          completed: false,
         },
         {
           id: 3,
-          text: 'Take a break',
-          completed: false
-        }
-      ]
-    }
+          text: "Take a break",
+          completed: false,
+        },
+      ],
+    };
   },
-methods: {
-  addTodo() {
-    this.todos.push({
-      id: this.todos.length + 1,
-      text: this.newTodo,
-      completed: false
-    })
-    this.newTodo = ''
+  methods: {
+    addTodo() {
+      if (this.newTodo.length == 0) {
+        return;
+      }
+      this.todos.push({
+        id: this.todos.length + 1,
+        text: this.newTodo,
+        completed: false,
+      });
+      this.newTodo = "";
+    },
+    deleteTodo(i) {
+      this.todos.splice(i, 1);
+    },
   },
-  deleteTodo(i){
-    this.todos.splice(i,1)
-  }
-}}
+};
 </script>
 
 <template>
   <div id="app">
     <div id="container">
-
       <h1>June Todo List:</h1>
-    <input type="text" class="input" placeholder="Add New Task" v-model="newTodo" />
-    <button class="add-button" v-on:click="addTodo()"  @click.enter="newTodo">Submit</button>
-   <!-- <div v-for="todo in todos" :key="todo-list"> -->
-<ul>
-       <li v-for="(todo,i) in todos">
-       <span>{{i + 1}} : {{todo.text}}</span>
-        <button class="del-button" @click="deleteTodo(i)">Delete</button>
-       </li>
-     </ul>
-        </div>
+      <div>
+        <input
+          type="text"
+          class="input"
+          placeholder="Add New Task"
+          v-model="newTodo"
+          v-on:keyup.enter="addTodo"
+        />
+        <button class="add-button" v-on:click="addTodo()">Submit</button>
+        <ul>
+          <li v-for="(todo, i) in todos">
+            <input
+              type="checkbox"
+              v-model="todo.completed"
+              @click="completed = !completed"
+              v-on:keyup.enter="completed"
+            />
+            <span>{{ i + 1 }}.{{ todo.text }}</span>
+
+            <button class="edit-button" @click="editTodo(i)">Edit</button>
+            <button
+              class="del-button"
+              @click="deleteTodo(i)"
+              v-on:keyup.delete="deleteTodo"
+            >
+              Delete
+            </button>
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
 <style>
-@import './assets/base.css';
-
+@import "./assets/base.css";
 </style>
